3f5ae65d967d870c2caf39d3769a3a5e6b7b7eb6
i.shirinov <shirinovilyos6965@gmail.com>
2022-11-29 15:55:21 +0500

    Task #4. Loop Keywords 2

--- src/main/java/school/mjc/stage0/loops/task4/FixMe.java
+++ src/main/java/school/mjc/stage0/loops/task4/FixMe.java
@@ -4,9 +4,9 @@ public class FixMe {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
             if (i % 2 == 0) {
-                System.out.print(i + " ");
+                System.out.print(i);
             } else if (i == 7) {
-                System.out.print("last ");
+                System.out.println("last ");
                 System.out.println("will I be printed?");
                 break;
             }
