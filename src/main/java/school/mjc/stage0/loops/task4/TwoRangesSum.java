e8e3b15c2804378d9e12985b9190696f6ff79970
i.shirinov <shirinovilyos6965@gmail.com>
2022-11-29 16:21:02 +0500

    Task #4. Loop Keywords 3

--- src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
+++ src/main/java/school/mjc/stage0/loops/task4/TwoRangesSum.java
@@ -2,22 +2,9 @@ package school.mjc.stage0.loops.task4;
 
 public class TwoRangesSum {
     public void printSumOfTwoRanges(int numberToSkip, int lastInRow) {
-        int sum1 = 0;
-        int sum2 = 0;
-        int x = numberToSkip;
-        for (int i = 1; i <= lastInRow; i++) {
-            numberToSkip--;
-            if (numberToSkip == 0) {
-                numberToSkip = x;
-                sum1 += i;
-                continue;
 
-            } else {
-                sum2 += i;
-            }
-        }
-        System.out.println(sum1);
-        System.out.println(sum2);
+        System.out.println((1 + lastInRow / numberToSkip) * (lastInRow / numberToSkip) / 2 * numberToSkip);
+        System.out.println((1 + lastInRow) * lastInRow / 2 - ((1 + lastInRow / numberToSkip) * (lastInRow / numberToSkip) /2* numberToSkip));
     }
 
     public static void main(String[] args) {
